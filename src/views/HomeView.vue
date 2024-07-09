792fe551028c9d32db5718947996bf98c8b1f8af
丘海涌 <1293044240@qq.com>
2024-07-09 13:36:47 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div id="app">
+      <VueDragResize :isActive="true" :w="200" :h="200">
+        <h3>Hello World!</h3>
+      </VueDragResize>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import VueDragResize from 'vue-drag-resize/src/component/vue-drag-resize.vue'
+</script>
+
+<style>
+.container {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
